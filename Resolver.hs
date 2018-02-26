b47ffe634e4ca820b5c1f9df37945c52cdf8b133
Vincent Hanquez <vincent@snarc.org>
2018-02-26 15:42:18 +0000

    add support for 8.4.1-rc1

--- Resolver.hs
+++ Resolver.hs
@@ -44,8 +44,24 @@ ghc84alpha2 = ResolverParameters "ghc-8.4-alpha2" compilerVer targets
         , Target "macosx" compilerVer (url "ghc-8.4.0.20180118-x86_64-apple-darwin.tar.xz") "b3119b255ab3d1a09fcf9919bddbbe2cd77f9175de14e4b23f20b40abe5edea1" 
         ]
 
+ghc84rc1 :: ResolverParameters
+ghc84rc4 = ResolverParameters "ghc-8.4-rc1" compilerVer targets
+  where
+    compilerVer = "8.4.0.20180224"
+    targets =
+        [ Target "windows64" compilerVer (url "ghc-8.4.0.20180224-x86_64-unknown-mingw32.tar.xz") "7d125b3bbaa2ec4022215787a259a41bf944ec02736c9555f2a19432eef21cad"
+        , Target "linux64" compilerVer (url "ghc-8.4.0.20180224-x86_64-unknown-linux.tar.xz") "34240b8055e81f78a58becd1351ce03235e1bb3fd14c419a7e97631514a780ad"
+        , Target "macosx" compilerVer (url "ghc-8.4.0.20180224-x86_64-apple-darwin.tar.xz") "4cf1afc5c6672e4628ba84c775a081fc4cf08bddd07f30fcc57d909fc5939c06"
+        , Target "windows32" compilerVer (url "ghc-8.4.0.20180224-i386-unknown-mingw32.tar.xz") "28799a8ceee197a050fa1ef0740a78b01bf93b13f5a7b8e7ef5061b231c270dc"
+        , Target "linux32-nopie" compilerVer (url "ghc-8.4.0.20180224-i386-deb8-linux.tar.xz") "ce2f7583c64b700b22281a488e9a950ee50ec756a83520e3360748f67e14bfa2"
+        , Target "linux64-nopie" compilerVer (url "ghc-8.4.0.20180224-x86_64-deb8-linux.tar.xz") "a83b57a600b513f689716e98b53badfb2e2aa1117bff7c5bab12c07854eecb07"
+        , Target "linux64-tinfo-nopie" compilerVer (url "ghc-8.4.0.20180224-x86_64-deb8-linux.tar.xz") "a83b57a600b513f689716e98b53badfb2e2aa1117bff7c5bab12c07854eecb07"
+        , Target "linux64-tinfo6-nopie" compilerVer (url "ghc-8.4.0.20180224-x86_64-fedora27-linux.tar.xz") "34240b8055e81f78a58becd1351ce03235e1bb3fd14c419a7e97631514a780ad"
+        ]
+
+
 extraResolvers :: [ResolverParameters]
-extraResolvers = [ ghc84alpha2 ]
+extraResolvers = [ ghc84alpha2, ghc84rc1 ]
 
 findExtraResolver :: String -> Maybe ResolverParameters
 findExtraResolver name = find (\(ResolverParameters n _ _) -> n == name) extraResolvers
